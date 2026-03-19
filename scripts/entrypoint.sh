32703d82976ad44d7d3b077a9c488713a59a6949
frogFred <tyne0917@gmail.com>
2026-03-19 22:58:57 +0800

    edit sh port

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -31,4 +31,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python scripts/bootstrap.py
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8001 --workers 3
